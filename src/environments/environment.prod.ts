1c6023e59a0009be69023add7a5e75ce2e75655e
Spenzy <zieddr@gmail.com>
2023-05-14 21:02:49 +0100

    dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  dataMiningAPI: "https://1b76-197-238-89-205.ngrok-free.app"
+};
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnuXW5vDCg_YYrVLn3MVNtYKVyMceRaSE",
+  authDomain: "activitylogmonitoring.firebaseapp.com",
+  databaseURL: "https://activitylogmonitoring-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "activitylogmonitoring",
+  storageBucket: "activitylogmonitoring.appspot.com",
+  messagingSenderId: "779549261919",
+  appId: "1:779549261919:web:d3f7d5055e04e615349bea",
+  measurementId: "G-JC69GG4354"
 };
